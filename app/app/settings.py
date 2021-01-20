8f4ffb233aec6acd123ffe040f1f12cdadfe823a
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-20 16:07:16 +0100

    deploy: try not speecifying static urls

--- app/app/settings.py
+++ app/app/settings.py
@@ -121,6 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = '/staticfiles/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
