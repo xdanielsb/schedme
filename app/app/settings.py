39df48e6b0fda846e4622a1d3dcdda48148676a5
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-20 16:09:12 +0100

    deploy: solve permission error

--- app/app/settings.py
+++ app/app/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
