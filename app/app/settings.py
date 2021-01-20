6199eb3a22fadaeecceee16ffe3978a563fe6e15
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-20 15:58:28 +0100

    deploy: test another setting

--- app/app/settings.py
+++ app/app/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/staticfiles/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
