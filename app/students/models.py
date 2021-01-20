2c49980aae9ad8597ab6c154b26adb1b5ca50078
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-20 13:03:00 +0100

    create functions to access calendar API

--- app/students/models.py
+++ app/students/models.py
@@ -1,13 +1,23 @@
 from django.db import models
-
+from ..common import calendar
 
 class Student(models.Model):
     name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
+    credentials = models.TextField(max_length=255)
 
     def __str__(self):
         return "{} {}".format(self.name, self.email)
+    
+    def getCredentials(self):
+        """
+        Obtain the user token from the database, generating it if it does not exist
+        """
+        if (self.token is None):
+            self.token = calendar.refresh_creds(self.token)
+            self.save()
+        return self.token
 
 
 class FreeTime(models.Model):
