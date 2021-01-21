6df3e63bc84e8f79149ece61c5b4e7b2d6903881
Clem2798 <clement.ribes@imt-atlantique.net>
2021-01-21 21:00:34 +0100

    Correction of a bug and allowing several teachers proposals to be used in a single student time slot (but no overlapping)

--- timetable_functions.py
+++ timetable_functions.py
@@ -53,13 +53,13 @@ def generate_random_slots(number):
     for i in range(number):
         beginning_time = randint(8,17)
         beginning = datetime.datetime(2021,1,19,beginning_time)
-        end = beginning+datetime.timedelta(hours=2)
+        end = beginning+datetime.timedelta(hours=3)
         slot = generate_slot(beginning, end)
         retured_list.append(slot)
     return retured_list
 
 def generate_student(id, name, likings, slots):
-    return {"id":id, "name": name, "likings":likings, "slots":slots}
+    return {"id":"s"+str(id), "name": name, "likings":likings, "slots":slots}
 
 def generate_random_student(id):
     dict_id = "s"+str(id)
@@ -77,7 +77,7 @@ def generate_random_students(number):
     for proposal in teacher["proposals"]:"""
 
 def is_free_time(teacher_proposal,student_slot):
-    return student_slot["beginning"] >= teacher_proposal["beginning"] and student_slot["end"] <= teacher_proposal["end"]
+    return student_slot["beginning"] <= teacher_proposal["beginning"] and student_slot["end"] >= teacher_proposal["end"]
 
 """def is_free_student(teacher_proposal,student):
     for student_slot in student["slots"]:
@@ -116,25 +116,26 @@ def print_teacher_list(teacher_list):
         print("\n")
 
 student_beginning_time = datetime.datetime(2021,1,19,8)
-student_ending_time = student_beginning_time+datetime.timedelta(hours=2)
+student_ending_time = student_beginning_time+datetime.timedelta(hours=3)
 
-teacher_beginning_time = datetime.datetime(2021,1,19,8)
-teacher_ending_time = student_beginning_time+datetime.timedelta(hours=3)
+teacher_beginning_time_1 = datetime.datetime(2021,1,19,8)
+teacher_ending_time_1 = teacher_beginning_time_1+datetime.timedelta(hours=2)
+
+teacher_beginning_time_2 = datetime.datetime(2021,1,19,10)
+teacher_ending_time_2 = teacher_beginning_time_2+datetime.timedelta(hours=1)
 
 student_slot = {"beginning":student_beginning_time, "end":student_ending_time}
-teacher_proposal = {"activity":"tennis", "beginning":teacher_beginning_time, "end":teacher_ending_time, "slots":2}
-student=generate_student(2,"Pete",["golf"],[student_slot])
+teacher_proposal_1 = {"activity":"tennis", "beginning":teacher_beginning_time_1, "end":teacher_ending_time_1, "number_of_slots":2}
+teacher_proposal_2 = {"activity":"tennis", "beginning":teacher_beginning_time_2, "end":teacher_ending_time_2, "number_of_slots":2}
+teacher_proposals = [teacher_proposal_1,teacher_proposal_2]
+teacher = {"id":"p2", "name":"Jane", "proposals":teacher_proposals}
+student=generate_student(2,"Pete",["tennis"],[student_slot])
 
 teacher_list = generate_random_teachers(5)
 student_list = generate_random_students(30)
 
-print("TEACHER LIST\n\n",teacher_list)
-
-print_teacher_list(teacher_list)
-
-print("STUDENT LIST\n\n")
-
 print_student_list(student_list)
+print_teacher_list(teacher_list)
 
 def match(teachers,students):
     match_list = []
@@ -148,13 +149,24 @@ def match(teachers,students):
                             if is_free_time(proposal,slot):
                                 match_list.append((teacher["id"],student["id"],proposal))
                                 student["slots"].remove(slot)
+                                if slot["beginning"] < proposal["beginning"]:
+                                    student["slots"].append({"beginning":slot["beginning"], "end":proposal["beginning"]})
+                                if slot["end"] > proposal["end"]:
+                                    student["slots"].append({"beginning":proposal["end"], "end":slot["end"]})
                                 proposal_slots -= 1
     return match_list
 
-matches = match(teacher_list,student_list)
+matches_one_test = match([teacher],[student])
+matches_test = match(teacher_list,student_list)
 
 def print_match_list(match_list):
     for match in match_list:
         print(str(match)+"\n")
 
-print_match_list(matches)
+print("With one teacher and one student:\n")
+
+print_match_list(matches_one_test)
+
+print("With several of both:\n")
+
+print_match_list(matches_test)
